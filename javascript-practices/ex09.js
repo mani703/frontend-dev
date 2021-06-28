ebb4c9b52c96308fc4de971333ac1cc9f41f01a9
unknown <mani703@naver.com>
2021-06-28 15:36:44 +0900

    javascript #5

--- javascript-practices/ex09.js
+++ javascript-practices/ex09.js
@@ -5,9 +5,24 @@
 var MyObject = function(name, age){
     this.name = name;
     this.age = age;
-}
-MyObject.prototypes.school = "bit";
-MyObject.prototypes.course = "douzone";
-MyObject.prototypes.info = function(){
+};
+MyObject.prototype.school = "bit";
+MyObject.prototype.course = "douzone";
+MyObject.prototype.info = function(){
     console.log(this.name + " : " + this.age + " : " + this.school + " : " + this.course);
 }
+
+// MyObject 객체 생성1
+var o1 = new MyObject("둘리", 10);
+o1.school = 'multicampus';
+o1.info();
+
+// MyObject 객체 생성2
+var o2 = new MyObject("마이콜", 20);
+o2.info();
+
+// 오버라이딩
+o2.info = function(){
+    console.log("비밀!!!");
+}
+o2.info();
