6382437f92e064d171ab26e67efa15016f2860c8
BIT <mani703@naver.com>
2021-07-06 17:49:01 +0900

    ajax #6 - Rest

--- ch08/src/main/java/com/douzone/frontdev/ch08/controller/TestController.java
+++ ch08/src/main/java/com/douzone/frontdev/ch08/controller/TestController.java
@@ -26,9 +26,19 @@ public class TestController {
 	public String json01() {
 		return "json01";
 	}
-	
+
 	@RequestMapping("/json02")
 	public String json02() {
 		return "json02";
+	}
+
+	@RequestMapping("/json03")
+	public String json03() {
+		return "json03";
+	}
+
+	@RequestMapping("/json04")
+	public String json04() {
+		return "json04";
 	}
 }
