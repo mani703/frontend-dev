0d7c3ea0ccc0b3a4abb7779c3de0b669df98fe9e
unknown <mani703@naver.com>
2021-07-19 15:23:26 +0900

    비동기 테스트

---
+++ javascript-practices/async/test/ex02.js
@@ -0,0 +1,14 @@
+const { assert } = require('chai');
+const ex02 = require('../ex02');
+// const assert = require('assert').strict;
+const should = require('chai').should();
+
+describe('ex02()', function(){
+    this.timeout(3000);
+
+    it('should return "ok"', async function(){
+        const res = await ex02('param-data');
+        // assert.equal(res, 'ok');
+        res.should.equal('ok');
+    });
+});
